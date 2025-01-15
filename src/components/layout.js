3ad14ff972b2283e547d951367705f063ace203c
Lucia <lucia.macakova@iths.se>
2025-01-15 16:45:48 +0100

    render Richtext, stick footer to bottom

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,10 @@
-
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
 
@@ -26,12 +14,11 @@ const Layout = ({ children }) => {
           maxWidth: `var(--size-content)`,
           padding: `var(--size-gutter)`,
         }}
-      >  <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      >  <Header />
         <main>
           {children}</main>
         <footer
           style={{
-            marginTop: `var(--space-5)`,
             fontSize: `var(--font-sm)`,
           }}
         >
--- src/templates/otherPages.js
+++ src/templates/otherPages.js
@@ -1,29 +1,104 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { graphql } from "gatsby";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
-import Navbar from "../components/Navbar";
 import Layout from "../components/layout";
+import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import * as styles from './otherPages.module.css';
 
 
 export const query = graphql`
 query ($slug: String!) {
   contentfulPage(slug: { eq: $slug }) {
     title
-    content {raw
+    content {
+      raw
+      references {
+        ... on ContentfulAsset {
+          contentful_id
+          title
+          description
+          gatsbyImageData(width: 500)
+          __typename
+        }
+          publicUrl
+      }
     }
   }
 }
 `;
 const Page = ({ data }) => {
-const { title, content } = data.contentfulPage;
-const options = {};
+  const { title, content } = data.contentfulPage;
 
-return (
-  <Layout>
-<h1>{title}</h1>
-<div>{renderRichText(content, options)}</div>
- </Layout>
-);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const checkMobile = () => {
+    if (window.matchMedia("(max-width: 576px)").matches) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+  console.log(content);
+
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+        const { gatsbyImageData, title } = node.data.target
+        return (
+          <div >
+            <GatsbyImage
+              image={getImage(gatsbyImageData)}
+              alt={title}
+            /></div>
+        )
+      },
+      [BLOCKS.PARAGRAPH]: (node) => {
+        return (
+          <div>
+            {node.content.map((child, index) => {
+              if (child.nodeType === 'text') {
+                return child.value;
+              }
+
+              if (child.nodeType === INLINES.HYPERLINK) {
+                const linkText = child.content.map((linkChild) => linkChild.value).join('');
+
+                return (
+                  <a
+                    href={linkText}
+                    target="_blank"
+                    key={index}
+                  >
+                    {linkText}
+                  </a>
+                );
+              }
+
+              return null;
+            })}
+          </div>
+        );
+      },
+    }
+  };
+
+
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <div className={isMobile ? styles.richTextContainerMobile : styles.richTextContainerDesktop}>        {renderRichText(content, options)}
+      </div>
+    </Layout>
+  );
 };
 
 export default Page;
